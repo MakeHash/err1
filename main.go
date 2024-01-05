8354fc02d2dbee8f8240349887e1bd63d1a8f9b7
MakeHash <directlytosuccess@gmail.com>
2024-01-05 11:31:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ main.go
@@ -0,0 +1,35 @@
+package inv
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Storage struct {
+	F string `json:"first"`
+	S int    `json:"second"`
+	// Add more fields as per your JSON data
+}
+
+func myServ(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		var data Storage
+
+		// Parse JSON from the request body to the struct
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("https://santiago.com/api", myServ)
+	err := http.ListenAndServe("https://santiago.com:8443", nil)
+	if err != nil {
+		fmt.Printf("error: %s", err)
+	}
+
+}
